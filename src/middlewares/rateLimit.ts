28445815a297f5d5c434f89a204cdc0f46f63815
arabyhosssainabid <arabyhossainabid@gmail.com>
2026-01-31 20:52:01 +0600

    Add rate limiting middleware

---
+++ src/middlewares/rateLimit.ts
@@ -0,0 +1,25 @@
+import rateLimit from 'express-rate-limit';
+
+export const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    statusCode: 429,
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+  },
+});
+
+export const authLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    statusCode: 429,
+    message: 'Too many login attempts from this IP, please try again after an hour',
+  },
+});
